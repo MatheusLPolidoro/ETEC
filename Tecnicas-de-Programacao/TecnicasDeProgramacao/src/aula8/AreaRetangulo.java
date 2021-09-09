00086d7a93450aa1d9bb09f222e6d43d6643ba57
Matheus Polidoro <matheus.polidoro@senaisp.edu.br>
2021-09-09 20:14:51 -0300

    exercicios iniciais de java

---
+++ Tecnicas-de-Programacao/TecnicasDeProgramacao/src/aula8/AreaRetangulo.java
@@ -0,0 +1,21 @@
+/*
+ * Crie um programa que calcule a área do retângulo abaixo e apresente
+ * o resultado na tela
+ */
+package aula8;
+
+public class AreaRetangulo {
+    public static void main(String[] args) {
+        int altura, base, area; // declaração de variáveis
+        
+        // entrada de dados (atribuição)
+        altura = 5;
+        base = 10;
+        
+        // processamento de dados
+        area = base * altura;
+        
+        // saída de dados
+        System.out.println("A área do retângulo é de: " + area);
+    }
+}
