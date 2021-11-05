da95187ba673bca0c72b2454e697242cd61b97d5
Ingrid Rodrigues <ingrid.rodrigues39@etec.sp.gov.br>
2021-11-05 09:29:02 -0300

    Exercicio aula 14 switch-case

---
+++ Tecnicas-de-Programacao/TecnicasDeProgramacao/src/aula14/ResultadoDeAluno.java
@@ -0,0 +1,24 @@
+package aula14;
+
+import java.util.Scanner;
+
+public class ResultadoDeAluno {
+    public static void main(String[] args) {
+        //Estrutura de decisão switch-case
+        Scanner entrada = new Scanner (System.in);
+        System.out.print("Informe a nota do aluno. ENTRE 0 E 10: ");
+        int nota = entrada.nextInt();
+        entrada.close();
+        
+        switch(nota) {
+            case 0:case 1:case 2:case 3: 
+                System.out.println("REPROVADO");break;
+            case 4:case 5:
+                System.out.println("RECUPERAÇÃO");break;
+            case 6:case 7:case 8:case 9:case 10:
+                System.out.println("APROVADO");break;
+            default:
+                System.out.println("Informe uma nota valida.");break;
+        }
+    }
+}
