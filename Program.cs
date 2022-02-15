9f3a968924628a0f6e0a9b2d8826df580a0e0ba8
Matheus <89528428+MatheusLPolidoro@users.noreply.github.com>
2022-02-14 21:55:49 -0300

    Aula_ds 14-02-2022

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Comentarios Lineares
+            /*
+             Comentarios
+             em
+             blocos
+            */
+            /*
+             Tipos de dados Primitivos:
+             String (Ex: a / A / b / B / 1 / 2 / ...) Armezana valores não números (Textos)
+             Int (Ex: 1 / 2 / 3 / 4 / 5) Armezana valores númericos inteiros
+             Float/Double (Ex: 1.1 / 1.556 / 256.3 / 5) Armazena valores númericos fracionados
+             Bool (Ex: True / False) Armazena valor verdeiro ou falso
+             */
+
+            int v1, v2, r;
+
+            Console.Write("Informe o primeiro valor: ");
+            v1 = int.Parse(Console.ReadLine());
+            Console.Write("Informe o segundo valor: ");
+            v2 = int.Parse(Console.ReadLine());
+
+            r = v1 + v2;
+
+            // Conversao implicita (AUTOMATICA) <> conversão explicita ()
+            Console.Write("Resultado: " + r.ToString());
+            Console.ReadKey();
+        }
+    }
+}
