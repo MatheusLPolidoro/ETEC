0b5005fcf7ce5f8a598716e7d37a33d0fbef81c2
Matheus Polidoro <mattpolidoro4@gmail.com>
2022-03-13 11:10:22 -0300

    atualizado

---
+++ Programacao-de-Aplicativos-Mobile/layout-10.03.2022/AppGestaoEscolar/AppGestaoEscolar/app/src/main/java/appgestaoescolar/principal/ActPrincipal.java
@@ -0,0 +1,14 @@
+package appgestaoescolar.principal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_principal);
+    }
+}
