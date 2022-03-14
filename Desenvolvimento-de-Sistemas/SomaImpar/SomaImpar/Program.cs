b6cb094370dd158d6d68cbdadfc465576d6a88e2
Matheus <89528428+MatheusLPolidoro@users.noreply.github.com>
2022-03-14 20:17:41 -0300

    Program Cs

---
+++ Desenvolvimento-de-Sistemas/SomaImpar/SomaImpar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomaImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int somaImpar;
+            somaImpar = 0;
+
+            for (num = 1; num <= 20; num += 2)
+            {
+                somaImpar += num;
+                Console.Write(num);
+                if (num < 19)
+                {
+                    Console.Write(" + ");
+                }
+                else
+                {
+                    Console.Write(" = ");
+                }
+            }
+            Console.WriteLine(somaImpar);
+            Console.ReadKey();
+        }
+    }
+}
