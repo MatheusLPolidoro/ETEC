13cedbba2a7e8ac5109fa9e5bfcc997df2511ec7
Matheus <89528428+MatheusLPolidoro@users.noreply.github.com>
2022-03-14 22:04:08 -0300

    Regex

--- Desenvolvimento-de-Sistemas/Calculadora/Calculadora/Program.cs
+++ Desenvolvimento-de-Sistemas/Calculadora/Calculadora/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Calculadora
 {
@@ -10,28 +11,46 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            int op;
-            float n1, n2;
+            string op;
+            double n1, n2;
             n1 = n2 = 0;
             do
             {
-                Console.WriteLine("1 - Somar");
-                Console.WriteLine("2 - Subtrair");
-                Console.WriteLine("3 - Multiplicar");
-                Console.WriteLine("4 - Dividir");
-                Console.WriteLine("5 - Sair");
+                do
+                {
+                    Console.WriteLine("1 - Somar");
+                    Console.WriteLine("2 - Subtrair");
+                    Console.WriteLine("3 - Multiplicar");
+                    Console.WriteLine("4 - Dividir");
+                    Console.WriteLine("5 - Exponenciação");
+                    Console.WriteLine("6 - Raiz Quadrada");
+                    Console.WriteLine("7 - Resto da divisão");
+                    Console.WriteLine("8 - Porcentagem");
+                    Console.WriteLine("9 - Sair");
+                    op = Console.ReadLine();
+                    if (Regex.IsMatch(op, @"^[0-9]+$"))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Opção inválida, informe um número! ");
+                        Console.ReadKey();
+                    }
+                    Console.Clear();
+                } while (true);
 
-                op = int.Parse(Console.ReadLine());
 
-                if (op > 0 && op < 5)
+
+                if (int.Parse(op) > 0 && int.Parse(op) < 9)
                 {
                     Console.WriteLine("Informe um número: ");
-                    n1 = float.Parse(Console.ReadLine());
+                    n1 = double.Parse(Console.ReadLine());
                     Console.WriteLine("Informe outro número: ");
-                    n2 = float.Parse(Console.ReadLine());
+                    n2 = double.Parse(Console.ReadLine());
                 }
 
-                switch (op)
+                switch (int.Parse(op))
                 {
                     case 1:
                         Console.WriteLine($"{n1.ToString("n")} + {n2.ToString("n")} = {(n1 + n2).ToString("n")}");
@@ -46,6 +65,18 @@ namespace Calculadora
                         Console.WriteLine($"{n1.ToString("n")} ÷ {n2.ToString("n")} = {(n1 / n2).ToString("n")}");
                         break;
                     case 5:
+                        Console.WriteLine($"{n1.ToString("n")} ^ {n2.ToString("n")} = {Math.Pow(n1, n2).ToString("n")}");
+                        break;
+                    case 6:
+                        Console.WriteLine($"√{n1.ToString("n")} = {Math.Sqrt(n1).ToString("n")}\n√{n2.ToString("n")} = {Math.Sqrt(n2).ToString("n")}");
+                        break;
+                    case 7:
+                        Console.WriteLine($"{n1.ToString("n")} : {n2.ToString("n")} = {(n1 % n2).ToString("n")}");
+                        break;
+                    case 8:
+                        Console.WriteLine($"{n1.ToString("n")} % de {n2.ToString("n")} = {(n1 / 100 * n2).ToString("n")}");
+                        break;
+                    case 9:
                         Console.WriteLine("Saindo...");
                         break;
                     default:
@@ -54,7 +85,7 @@ namespace Calculadora
                 }
                 Console.ReadKey();
                 Console.Clear();
-            } while (op != 5);
+            } while (int.Parse(op) != 9);
         }
     }
 }
