3f82f028cb6bc335ea9d0fe9e560e14a61cb7b47
Matheus Polidoro <mattpolidoro4@gmail.com>
2022-04-03 16:11:20 -0300

    css php

--- Programacao-Web/carro/index.php
+++ Programacao-Web/carro/index.php
@@ -4,55 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width='device-width', initial-scale=1.0">
     <title>Consulta Carro</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<style>
-    body{
-        margin-top: 8%;
-        margin: 50 auto;
-        align:center;
-        text-align:center;
-    }
-    h1{
-        color: #4CAF50;
-        font-family: Arial, Helvetica, sans-serif;
-    }
-    form{
-        /* topo | direita | inferior | esquerda */
-        margin: 0 40% 0 40%;
-        padding: 2%;
-        border-style: solid;
-        border-color: #4CAF50;
-        border-radius: 10px;
-        transition: all ease .2s;
-    }
-    .botao {
-        background-color: #4CAF50; /* Green */
-        border: none;
-        color: white;
-        padding: 10px 32px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        transition: all ease .2s;
-        width: 75%px
-    }
-    select, input{
-        padding: 5px;
-        font-size: 16px;
-        width: 70%;
-    }
-    .botao:hover {
-        background-color: #4C50AF; /* blue */
-        transition: all ease .2s;
-    }
-    form:hover{
-        border-color: #4C50AF; /* blue */
-        transition: all ease .2s;
-    }
-</style>
 <body>
-    <h1>Informe sua placa</h1>
+    <h1>SELECIONE O FINAL DA SUA PLACA</h1>
     <br><br>
     <form action="recebe.php" method="post">
         <select name="placa">
--- Programacao-Web/carro/recebe.php
+++ Programacao-Web/carro/recebe.php
@@ -16,69 +16,84 @@
     {
         case 0:
             $mes_limite = "DEZEMBRO";
-            $dia_rodizio = "Sexta-feira";
+            $dia_rodizio = "SEXTA-FEIRA";
             $calendario_ipva = ["21/Jan", "21/Fev", "23/Mar"];
             break;
         case 1:
             $mes_limite = "JULHO";
-            $dia_rodizio = "Segunda-feira";
+            $dia_rodizio = "SEGUNDA-FEIRA";
             $calendario_ipva = ["24/Jan", "23/Fev", "25/Mar"];        
             break;
         case 2:
             $mes_limite = "JULHO";
-            $dia_rodizio = "Segunda-feira";
+            $dia_rodizio = "SEGUNDA-FEIRA";
             $calendario_ipva = ["25/Jan", "24/Fev", "28/Mar"];        
             break;
         case 3:
             $mes_limite = "AGOSTO";
-            $dia_rodizio = "Terça-feira";
+            $dia_rodizio = "TERÇA-FEIRA";
             $calendario_ipva = ["26/Jan", "25/Fev", "29/Mar"];        
             break;
         case 4:
             $mes_limite = "AGOSTO";
-            $dia_rodizio = "Terça-feira";
+            $dia_rodizio = "TERÇA-FEIRA";
             $calendario_ipva = ["27/Jan", "03/Mar", "04/Abr"];        
             break;
         case 5:
             $mes_limite = "SETEMBRO";
-            $dia_rodizio = "Quarta-feira";
+            $dia_rodizio = "QUARTA-FEIRA";
             $calendario_ipva = ["28/Jan", "04/Mar", "05/Abr"];        
             break;
         case 6:
             $mes_limite = "SETEMBRO";
-            $dia_rodizio = "Quarta-feira";
+            $dia_rodizio = "QUARTA-FEIRA";
             $calendario_ipva = ["31/Jan", "07/Mar", "06/Abr"];        
             break;
         case 7:
             $mes_limite = "OUTUBRO";
-            $dia_rodizio = "Quinta-feira";
+            $dia_rodizio = "QUINTA-FEIRA";
             $calendario_ipva = ["01/Fev", "08/Mar", "07/Abr"];        
             break;
         case 8:
             $mes_limite = "OUTUBRO";
-            $dia_rodizio = "Quinta-feira";
+            $dia_rodizio = "QUINTA-FEIRA";
             $calendario_ipva = ["02/Fev", "09/Mar", "08/Abr"];        
             break;
         case 9:
             $mes_limite = "NOVEMBRO";
-            $dia_rodizio = "Sexta-feira";
+            $dia_rodizio = "SEXTA-FEIRA";
             $calendario_ipva = ["03/Fev", "10/Mar", "11/Abr"];        
             break;        
         default:
             echo "Numero inválido";
             break;
     }
-    echo "<form>";
-    echo "<div class='resultado'>Mês limite pagar o licenciamento:$mes_limite</div>";
-    echo "<div class='resultado'>Dia do rodízio SP:$dia_rodizio</div>";
-    echo "<div class='resultado'>Calendário IPVA:</div>";
-    for ($i = 0; $i < 3; $i++)
-    {
-        $parcela = $i + 1;
-        echo "<div class='resultado'>$parcela ° parcela: $calendario_ipva[$i]</div>";
-    }
-    echo "</form>";
-    ?>    
+    ?>
+    <a href="index.php">&#8617;</a>
+    <img src="images/carros.png" alt="Carros">
+    <table>
+        <tr>
+            <td>MÊS LIMITE <br>PARA PAGAMENTO<br> DO LICENCIAMENTO</td>
+            <td colspan="2"><?php echo $mes_limite ?></td>
+        </tr>
+        <tr>
+            <td>DIA DO RODÍZIO SP</td>
+            <td colspan="2"><?php echo $dia_rodizio ?></td>
+        </tr>
+        <tr>
+            <td rowspan="3">CALENDARIO IPVA</td>
+            <td>1° PARCELA</td>
+            <td><?php echo $calendario_ipva[0] ?></td>
+        </tr>
+        <tr>
+            <td>2° PARCELA</td>
+            <td><?php echo $calendario_ipva[1] ?></td>
+        </tr>
+        <tr>
+            <td>3° PARCELA</td>
+            <td><?php echo $calendario_ipva[2] ?></td>
+        </tr>
+    </table>
 </body>
 </html>
 
