a36b35971ceb996de72b54ef3c01f362457d1aa9
matheuslpolidoro <mattpolidoro4@gmail.com>
2022-04-04 19:42:26 -0300

    Orientacao a objeto 28-03

---
+++ Desenvolvimento-de-Sistemas/ex01-28-03/ExemploOrientacao/Aluno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploOrientacao
+{
+    class Aluno
+    {
+        //Atributos (Caracteristicas)
+
+        public string nome;
+        public int idade;
+        public string curso;
+
+        //Metodos (Acoes)
+        public void ResponderChamada()
+        {
+            Console.WriteLine("Presente !");
+        }
+    }
+}
---
+++ Desenvolvimento-de-Sistemas/ex01-28-03/ExemploOrientacao/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploOrientacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciar um objeto aos "moldes" da classe (fazer o pudim)
+            //Instanciar --> Criar em Mem. Ram o objeto
+            //Sintaxe: NomeDaClasse nomeDoObjeto = new MetodoConstrutor();
+            Aluno a = new Aluno();
+
+            Console.Write("Informe o nome do aluno: ");
+            a.nome = Console.ReadLine();
+
+            Console.WriteLine("O professor chama: ");
+            Console.WriteLine(a.nome + " esta presente ?!");
+
+            Console.WriteLine("E o aluno responde:");
+            a.ResponderChamada();
+
+            Console.ReadKey();//Pausa final
+        }
+    }
+}
