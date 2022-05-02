43beed5052d04fd378f74195ac6450ec457f8a2c
matheuslpolidoro <mattpolidoro4@gmail.com>
2022-05-02 20:31:41 -0300

    Aula 02-05-2022

---
+++ Desenvolvimento-de-Sistemas/componentesVisuais/componentesVisuais/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace componentesVisuais
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOlaMundo = new System.Windows.Forms.Button();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOlaMundo
+            // 
+            this.btnOlaMundo.Location = new System.Drawing.Point(157, 165);
+            this.btnOlaMundo.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnOlaMundo.Name = "btnOlaMundo";
+            this.btnOlaMundo.Size = new System.Drawing.Size(138, 42);
+            this.btnOlaMundo.TabIndex = 0;
+            this.btnOlaMundo.Text = "Clique aqui!";
+            this.btnOlaMundo.UseVisualStyleBackColor = true;
+            this.btnOlaMundo.Click += new System.EventHandler(this.btnOlaMundo_Click);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(32, 36);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(168, 24);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Digite o seu nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(36, 63);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(192, 29);
+            this.txtNome.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 222);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.btnOlaMundo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOlaMundo;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtNome;
+    }
+}
+
