5922006953cef5bf70d0c4aaa9af82e0dc971b78
Matheus Polidoro <mattpolidoro4@gmail.com>
2022-05-11 23:29:10 -0300

    Atualização

--- Programacao-Web/aula_11-05/matriz.php
+++ Programacao-Web/aula_11-05/matriz.php
@@ -15,7 +15,7 @@
     $quiz[0][2] =  'uma placa mae';
     $quiz[0][3] =  'um software';
     $quiz[0][4] =  'um espaco reservado na memoria';
-    $quiz[0][5] =  'd';
+    $quiz[0][5] =  'D';
     
     $quiz[1][0] =  'Para que a transferência de dados na Internet seja realizada, o protocolo HTTP necessita estar agregado a outros dois protocolos de rede, quais são esses protocolos?';
 
@@ -23,7 +23,7 @@
     $quiz[1][2] =  'HTTPS e HTML';
     $quiz[1][3] =  'WWW e HTML';
     $quiz[1][4] =  'TCP e IP';
-    $quiz[1][5] =  'd';
+    $quiz[1][5] =  'D';
 
     $quiz[2][0] =  'Para que a transferência de dados na Internet seja realizada, o protocolo HTTP necessita estar agregado a outros dois protocolos de rede, quais são esses protocolos?';
 
@@ -31,7 +31,7 @@
     $quiz[2][2] =  'HTTPS e HTML';
     $quiz[2][3] =  'WWW e HTML';
     $quiz[2][4] =  'TCP e IP';
-    $quiz[2][5] =  'd';
+    $quiz[2][5] =  'A';
 
     ?>
 
@@ -39,23 +39,24 @@
         <?php 
         $questao = 1;
         $pontos = 0;
-        for ($col=0; $col < 3; $col++) {
+        for ($row=0; $row < 3; $row++) {
             $op_i = 0;
-            echo $questao.' - '.$quiz[$col][0].'<br>';
-            for ($row=1; $row < 5; $row++) { 
-                echo "<input type='radio' name='$questao' value='$op[$op_i]' required>".$op[$op_i].') '.$quiz[$col][$row].'<br>';
+            echo $questao.' - '.$quiz[$row][0].'<br>';
+            for ($col=1; $col < 5; $col++) { 
+                echo "<input type='radio' name='$questao' value='$op[$op_i]' required>".$op[$op_i].') '.$quiz[$row][$col].'<br>';
                 $op_i++;
             }
             // recebe as respostas do client
             if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                if ($_POST[$questao] == "4"){
-                    echo $_POST[$questao];
+                if ($_POST[$questao] == $quiz[$row][5]){
                     $pontos++;
                 }
             }
             echo '<br><br>';
             $questao++;
-            echo $pontos;
+            if ($row == 2) {
+                echo $pontos;
+            }
         }
         ?>
         <input type="submit">
